d546e9aef6b4444cef6246ace1218e2204fa2f1d
Ali Hefny <ahefny@ejada.com>
2024-01-22 17:27:25 +0200

    create TopTabs

---
+++ src/components/organisms/TopTabs/TopTabs.tsx
@@ -0,0 +1,103 @@
+import { t } from "i18next";
+import React, { useRef, useEffect, useState, memo } from "react";
+
+import styles from "./TopTabs.styles";
+import {
+  FlatList,
+  Dimensions,
+  Platform,
+  I18nManager,
+  View,
+  Pressable,
+  Text,
+} from "react-native";
+import { colors } from "@constants/styles";
+
+type ITap = {
+  id: number;
+  name: string;
+};
+
+type props = {
+  Tabs: ITap[];
+  onPressFn: Function;
+  activeTab: number;
+  initialTab?: number;
+};
+
+const FilterScrollTabs = ({
+  Tabs,
+  onPressFn = () => {},
+  activeTab = 0,
+  initialTab = 0,
+}: props) => {
+  const flatListRef = useRef<FlatList>(null);
+  const [tabWidth, setTabWidth] = useState<string | number>("auto");
+  const adjustScreenWidth = () => {
+    const containerWidth = Dimensions.get("window").width;
+    const calculatedWidth = containerWidth / Tabs.length;
+    setTabWidth(calculatedWidth);
+  };
+
+  useEffect(() => {
+    adjustScreenWidth();
+
+    // if (Tabs?.length === 2) {
+    //   adjustScreenWidth();
+    // } else {
+    //   setTabWidth("auto");
+    // }
+  }, [Tabs]);
+
+  return (
+    <View>
+      <FlatList
+        ref={flatListRef}
+        contentContainerStyle={{ width: "100%", backgroundColor: "red" }}
+        onScrollToIndexFailed={(err) => {
+          console.log(err);
+        }}
+        data={Tabs}
+        showsHorizontalScrollIndicator={false}
+        horizontal
+        initialScrollIndex={initialTab}
+        renderItem={({ item: tab, index }) => {
+          return (
+            <Pressable
+              onPress={() => {
+                onPressFn(tab.name, tab.id);
+                flatListRef.current?.scrollToIndex({
+                  animated: true,
+                  index:
+                    I18nManager.isRTL && Platform.OS === "ios"
+                      ? Tabs?.length - 1 - index
+                      : index,
+                  viewPosition: Platform.OS === "ios" ? 1 : 0.5,
+                });
+              }}
+              style={[styles.container, { width: tabWidth }]}
+            >
+              <View
+                style={[
+                  activeTab === tab.id ? styles.isPressed : {},
+                  styles.tab,
+                ]}
+              >
+                <Text
+                  style={{
+                    color: activeTab === tab.id ? colors.primary : "black",
+                    textAlign: "center",
+                  }}
+                >
+                  {tab.name}
+                </Text>
+              </View>
+            </Pressable>
+          );
+        }}
+      />
+    </View>
+  );
+};
+
+export default FilterScrollTabs;
