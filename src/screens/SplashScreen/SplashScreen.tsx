03d5b5f167742778977e37cf4cd4c45fe49763aa
Ali Hefny <ahefny@ejada.com>
2024-01-23 01:12:49 +0200

    add splash screen with logo animation

--- src/screens/SplashScreen/SplashScreen.tsx
+++ src/screens/SplashScreen/SplashScreen.tsx
@@ -1,21 +1,23 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { useNavigation } from "@react-navigation/native";
+import LottieView from "lottie-react-native";
 
 const SplashScreen = () => {
   const navigation = useNavigation();
 
   setTimeout(() => {
     navigation.navigate("Home");
-  }, 100);
+  }, 4000);
 
   return (
-    <View style={{ flex: 1 }}>
-      <Text>SplashScreen</Text>
-      <Text>SplashScreen</Text>
-      <Text>SplashScreen</Text>
-      <Text>SplashScreen</Text>
-    </View>
+    <>
+      <LottieView
+        source={require("./discotech.json")} // Path to your Lottie animation file
+        autoPlay
+        loop
+      />
+    </>
   );
 };
 
